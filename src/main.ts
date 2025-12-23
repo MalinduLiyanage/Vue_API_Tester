288115e0b1609b5561ee2210f9534cb8446a95e5
MalinduLiyanage <malindumadhubashana@gmail.com>
2025-12-23 16:41:39 +0530

    development - Material Icons Import

--- src/main.ts
+++ src/main.ts
@@ -7,10 +7,15 @@ import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
 
+import '@mdi/font/css/materialdesignicons.css'
+
 import App from './App.vue'
 import router from './router'
 
 const vuetify = createVuetify({
+  icons: {
+    defaultSet: 'mdi',
+  },
   components,
   directives,
 })
