1c3566f3c5fde094fed7dcd621edbe7541289153
MalinduLiyanage <malindumadhubashana@gmail.com>
2025-12-28 17:34:18 +0530

    development - Component display for request and responses for an endpoint

---
+++ src/components/EndpointObjectBody.vue
@@ -0,0 +1,40 @@
+﻿<template>
+  <v-container class="ma-0">
+    <v-row>
+      <h4>Request Body</h4>
+      {{ endpointBody.requestBody }}
+    </v-row>
+
+    <v-row>
+      <h4>Response</h4>
+      {{ endpointBody.responses }}
+    </v-row>
+  </v-container>
+</template>
+
+<script lang="ts">
+import {defineComponent, type PropType} from 'vue'
+import type {Operation} from "@/helpers/classes/dtos/openapi-version-3-0-x/paths/operation.ts";
+
+export default defineComponent({
+  name: "EndpointObjectBody",
+  props:{
+    endpointBody: {
+      type: Object as PropType<Operation>,
+      required: true
+    }
+  },
+  watch:{
+    endpointBody:{
+      immediate: true,
+      handler(newVal){
+        console.info('EndpointObjectBody - endpointBody changed:', newVal)
+      }
+    }
+  },
+})
+</script>
+
+<style scoped>
+
+</style>
