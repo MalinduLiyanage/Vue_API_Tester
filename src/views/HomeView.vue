4670308476df8676a1de86f760d19b9889de3b3c
MalinduLiyanage <malindumadhubashana@gmail.com>
2025-12-28 17:35:03 +0530

    development - Parse the JSON and populate on ApiEndpointObject Array

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,5 +1,6 @@
 ﻿<template>
   <v-container>
+    <span>Paste Open API v3.0.x JSON</span>
     <v-textarea
       v-model="jsonString"
       label="Enter JSON"
@@ -8,27 +9,44 @@
     ></v-textarea>
   </v-container>
   <v-btn @click="onJsonInput">OK</v-btn>
+  <ApiEndpointObject
+    v-for="(item, index) in pathItems"
+    :key="index"
+    :endpointObject="item"
+  />
+
 </template>
 
 <script lang="ts">
 import {defineComponent} from 'vue'
 import {OpenAPIDocument} from "@/helpers/classes/dtos/openapi-version-3-0-x/root/openAPIDocument.ts";
 import OpenApiUtil from "@/helpers/utils/openApiUtil.ts";
+import ApiEndpointObject from "@/components/ApiEndpointObject.vue";
+import {PathItem} from "@/helpers/classes/dtos/openapi-version-3-0-x/paths/pathItem.ts";
 
 export default defineComponent({
   name: "HomeView",
+  components: {ApiEndpointObject},
   data(){
     return{
       jsonObj: new OpenAPIDocument(),
       jsonString: OpenApiUtil.stringifyDocument(this.jsonObj),
+      pathItems: [] as PathItem[]
     }
   },
   methods:{
     onJsonInput() {
       try {
-        console.info(OpenApiUtil.logInputDocument(this.jsonString))
+        // Parse the JSON string from textarea
+        const parsedDoc = OpenApiUtil.logInputDocument(this.jsonString);
+        console.info('Parsed Document:', parsedDoc);
+
+        // Now call orderByTags with the parsed document
+        const groupedByTags = OpenApiUtil.orderByTags(parsedDoc);
+        console.info('Grouped by Tags:', groupedByTags);
+        this.pathItems = groupedByTags;
       } catch (e) {
-        console.warn('Invalid JSON input', e)
+        console.warn('Invalid JSON input', e);
       }
     }
   },
